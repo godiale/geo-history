7fbb286606b0c15afe21ce0cfc46ef843f9d7bea
godiale <11892279+godiale@users.noreply.github.com>
2022-10-15 12:48:58 +0200

    Read excel using Pandas

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import pandas as pd
+
+
+def main():
+    df = pd.read_excel(io='input/Berlin.xlsx')
+    df = df[['Address', 'Viewed', 'Applied']]
+    print(df)
+
+
 if __name__ == '__main__':
-    print("Hello, geo-history")
+    main()
